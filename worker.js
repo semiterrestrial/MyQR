eed3733d96b16388c9d6f4e06b62141e91aae61d
Øystein Øvrebø <oystein.ovrebo@frende.no>
2021-01-26 21:32:37 +0100

    worker

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = "offline";
 // Customize this with a different URL if needed.
-const OFFLINE_URL = "offline.html";
+const OFFLINE_URL = "display.html";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
